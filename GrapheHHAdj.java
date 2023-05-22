0bf3440b05909fc84c233b309bdebdf890395507
faust332 <129494376+faust332@users.noreply.github.com>
2023-05-22 14:33:34 +0200

    GrapheHHAdj.java

--- GrapheHHAdj.java
+++ GrapheHHAdj.java
@@ -1,22 +1,21 @@
-package graphe;
+package implems;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GrapheHHAdj extends groupe {
 	private Map<String, Map<String, Integer>> hhadj;
-	private List<String> LisSom;
 	public GrapheHHAdj() {
 		hhadj = new HashMap<String, Map<String, Integer>>();
-		LisSom = new ArrayList<>();
 	}
 	
 	@Override
 	public List<String> getSommets() {
 		// TODO Auto-generated method stub
-		return this.LisSom;
+		return new ArrayList<>(hhadj.keySet());
 	}
 
 	@Override
@@ -25,7 +24,7 @@ public class GrapheHHAdj extends groupe {
 	    if (hhadj.containsKey(sommet)) { // check if the key exists in the map
 	        return new ArrayList<String>(hhadj.get(sommet).keySet());
 	    }
-		return null;
+		return Collections.emptyList();
 	}
 
 	@Override
@@ -59,7 +58,6 @@ public class GrapheHHAdj extends groupe {
 		// TODO Auto-generated method stub
 		if (!contientSommet(noeud)) {
 			hhadj.put(noeud, new HashMap<>());
-			LisSom.add(noeud);
 		}
 	}
 
@@ -71,27 +69,16 @@ public class GrapheHHAdj extends groupe {
 			if ( valeur < 0) {
 				throw new IllegalArgumentException("valeur doit être positive");
 			}
-			else {
-				if(!contientSommet(source)) {
-					ajouterSommet(source);
-				}
-				if(!contientSommet(destination)) {
-					ajouterSommet(destination);
-				}
-				// matrice prend la nouvel valeur de la source à la destination
-				try {
-				    // code that may throw the IllegalArgumentException
-
-					if(contientArc(source, destination)) {
-						throw new IllegalArgumentException("Arc existe déjà");
-					}
-					else {
-						hhadj.get(source).put(destination, valeur);
-					}
-				} catch (IllegalArgumentException e) {
-				    throw e;
-				}
+			
+			if(contientArc(source, destination)) {
+				throw new IllegalArgumentException("Arc existe déjà");
 			}
+			else {
+				ajouterSommet(source);
+				ajouterSommet(destination);
+				hhadj.get(source).put(destination, valeur);
+			}
+			
 		} catch (IllegalArgumentException e) {
 		    throw e;
 		}
@@ -99,7 +86,6 @@ public class GrapheHHAdj extends groupe {
 
 	@Override
 	public void oterSommet(String noeud) {
-		// TODO Auto-generated method stub
 		if (contientSommet(noeud)) {
 			List<String> lis = getSucc(noeud);
 			//supprime tous les arcs du sommet
@@ -107,18 +93,16 @@ public class GrapheHHAdj extends groupe {
 				oterArc(noeud, lis.get(i));
 			}
 			//suprime l'indices noeuds
-			LisSom.remove(noeud);
 			hhadj.remove(noeud);
 		}
 	}
 
 	@Override
 	public void oterArc(String source, String destination) {
-		// TODO Auto-generated method stub
 		try {
 		    // code that may throw the IllegalArgumentException
 			if (!contientArc(source, destination)) {
-				throw new IllegalArgumentException("valeur doit être positive");
+				throw new IllegalArgumentException("l'arc n'existe pas");
 			}
 			else {
 				hhadj.get(source).remove(destination);				
@@ -127,5 +111,6 @@ public class GrapheHHAdj extends groupe {
 		    throw e;
 		}
 	}
+	
 	
 }
